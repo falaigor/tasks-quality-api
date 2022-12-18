9cd679f9a58a571bdb9acd50c570362312ea7afb
Igor Santos <falaigors@gmail.com>
2022-12-17 23:35:02 -0300

    refactory: delete files

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
-import { AppModule } from 'src/modules/app.module';
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const prismaService: PrismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-
   app.enableCors();
-
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(3000);
 }
