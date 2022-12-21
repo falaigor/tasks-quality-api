cb31cc57f4c70915564b76e850043e0da6e08926
Igor Santos <falaigors@gmail.com>
2022-12-21 13:37:34 -0300

    feat: create endpoint to get count tasks from user

--- src/infra/http/task/controllers/tasks.controller.ts
+++ src/infra/http/task/controllers/tasks.controller.ts
@@ -3,10 +3,26 @@ import { CreateTaskBody } from '../dtos/create-task-body';
 import { TaskViewModel } from '../view-models/task.view-model';
 import { AddTask } from '@application/use-cases/task/add-task';
 import { GetUserTasks } from '@application/use-cases/task/get-user-tasks';
+import { CountUserTasks } from '@application/use-cases/task/count-user-tasks';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private addTask: AddTask, private getUserTasks: GetUserTasks) {}
+  constructor(
+    private addTask: AddTask,
+    private getUserTasks: GetUserTasks,
+    private countUserTasks: CountUserTasks,
+  ) {}
+
+  @Get('count/from/:userId')
+  async countFromUser(
+    @Param('userId') userId: string,
+  ): Promise<{ count: number }> {
+    const count = await this.countUserTasks.execute({
+      userId,
+    });
+
+    return count;
+  }
 
   @Get('from/:userId')
   async getFromUser(@Param('userId') userId: string) {
@@ -14,7 +30,9 @@ export class TasksController {
       userId,
     });
 
-    return tasks.map(TaskViewModel.toHTTP);
+    return {
+      tasks: tasks.map(TaskViewModel.toHTTP),
+    };
   }
 
   @Post()
--- src/infra/http/task/task.module.ts
+++ src/infra/http/task/task.module.ts
@@ -3,10 +3,11 @@ import { DatabaseModule } from '@infra/database/database.module';
 import { TasksController } from './controllers/tasks.controller';
 import { AddTask } from '@application/use-cases/task/add-task';
 import { GetUserTasks } from '@application/use-cases/task/get-user-tasks';
+import { CountUserTasks } from '@application/use-cases/task/count-user-tasks';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [TasksController],
-  providers: [AddTask, GetUserTasks],
+  providers: [AddTask, GetUserTasks, CountUserTasks],
 })
 export class TaskModule {}
