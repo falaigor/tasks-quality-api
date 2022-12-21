95475d936ff984695e80b3abd0006e62657c18ed
Igor Santos <falaigors@gmail.com>
2022-12-21 13:32:36 -0300

    feat: create controller from get tasks from userId

--- src/infra/http/task/controllers/tasks.controller.ts
+++ src/infra/http/task/controllers/tasks.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskBody } from '../dtos/create-task-body';
 import { TaskViewModel } from '../view-models/task.view-model';
 import { AddTask } from '@application/use-cases/task/add-task';
+import { GetUserTasks } from '@application/use-cases/task/get-user-tasks';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private addTask: AddTask) {}
+  constructor(private addTask: AddTask, private getUserTasks: GetUserTasks) {}
+
+  @Get('from/:userId')
+  async getFromUser(@Param('userId') userId: string) {
+    const { tasks } = await this.getUserTasks.execute({
+      userId,
+    });
+
+    return tasks.map(TaskViewModel.toHTTP);
+  }
 
   @Post()
   async create(@Body() body: CreateTaskBody) {
--- src/infra/http/task/task.module.ts
+++ src/infra/http/task/task.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { TasksController } from './controllers/tasks.controller';
 import { AddTask } from '@application/use-cases/task/add-task';
+import { GetUserTasks } from '@application/use-cases/task/get-user-tasks';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [TasksController],
-  providers: [AddTask],
+  providers: [AddTask, GetUserTasks],
 })
 export class TaskModule {}
