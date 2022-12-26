b887cc5d0129bca61712d1ff4649cf7805bf7d3c
Igor Santos <falaigors@gmail.com>
2022-12-26 17:05:37 -0300

    fix: update scope name

--- src/infra/http/auth/controllers/auth.controller.ts
+++ src/infra/http/auth/controllers/auth.controller.ts
@@ -1,13 +1,10 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
-import { AuthService } from '../services/auth.service';
+import { Body, Controller, Post } from '@nestjs/common';
+
 import { LoginGithub } from '@application/use-cases/auth/login-github';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-    private loginGithub: LoginGithub,
-  ) {}
+  constructor(private loginGithub: LoginGithub) {}
 
   @Post('session/github')
   async loginWithGithub(@Body() body: { code: string }) {
@@ -15,11 +12,4 @@ export class AuthController {
 
     return await this.loginGithub.execute(code);
   }
-
-  // @Post('session/google')
-  // async loginGoogle(@Body() body: { access_token: string }) {
-  //   const { access_token } = body;
-
-  //   return await this.authService.execute(access_token);
-  // }
 }
--- src/infra/http/auth/services/auth.service.ts
+++ src/infra/http/auth/services/auth.service.ts
@@ -15,6 +15,4 @@ export class AuthService {
 
     return null;
   }
-
-  //https://accounts.google.com/o/oauth2/v2/auth
 }
